5b2d3594c61afad32919c144b0b0a1e903026df1
tanlinfeng <1828765367@qq.com>
2024-03-07 23:21:25 +0800

    feat.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
 #include <string>
-#include "SFML"
+
+using namespace std;
 
 int main()
 {
     std::string s = "Hello, World!";
     std::cout << s << std::endl;
+
+    std::byte sefsef;
+
+//     // sf::CircleShape shape(100.f);
+//     // shape.setFillColor(sf::Color::Green);
+
+//     while (window.isOpen())
+//     {
+//         sf::Event event;
+//         while (window.pollEvent(event))
+//         {
+//             if (event.type == sf::Event::Closed)
+//                 window.close();
+//         }
+
+//         window.clear();
+//         // window.draw(shape);
+//         window.display();
+//     }
+
+
     return 0;
 }
+
+
+
+// int main()
+// {
+//     sf::RenderWindow window(sf::VideoMode(200, 200), "title");
+//     // sf::CircleShape shape(100.f);
+//     // shape.setFillColor(sf::Color::Green);
+
+//     while (window.isOpen())
+//     {
+//         sf::Event event;
+//         while (window.pollEvent(event))
+//         {
+//             if (event.type == sf::Event::Closed)
+//                 window.close();
+//         }
+
+//         window.clear();
+//         // window.draw(shape);
+//         window.display();
+//     }
+
+//     return 0;
+// }
